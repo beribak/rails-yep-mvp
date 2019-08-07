9daa51975eb3b1de1d646b774d7d5e2bbd4cc6e9
danko beribak <beribakdanko@gmail.com>
2019-08-07 16:45:39 +0200

    rough yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      'ch High St, London E1 6PQ',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza West',
+    address:      'gobl goble',
+    category:     'belgian'
+  },
+  {
+    name:         'Pizza North',
+    address:      'rick and morty',
+    category:     'french'
+  },
+  {
+    name:         'Pizza South',
+    address:      '6A Shoreditch High St, London E1 6PQ',
+    category:     'chinese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
